8d33ae8ad5a9f82ec1f2ef03d42eaf2c0a973499
Siraj <106668570+Sirajmd1@users.noreply.github.com>
2026-08-20 10:38:46 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -30,7 +30,7 @@ module "eks" {
   vpc_id              = module.vpc.vpc_id
   private_subnet_ids  = module.vpc.private_subnet_ids
   public_subnet_ids   = module.vpc.public_subnet_ids
-  node_instance_types = ["t3.medium"]
+  node_instance_types = ["c7i-flex.large"]
   capacity_type       = "ON_DEMAND" # 💰 switch to ON_DEMAND only if Spot capacity is unavailable
   desired_size        = 1
   min_size            = 1
